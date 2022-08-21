1d87072174f5fdf3268b2ddc309e79f33a636f83
Ajay_Bains <ajay.bains@codvo.ai>
2022-08-21 20:20:31 +0530

    wrote few more tests

--- script.test.js
+++ script.test.js
@@ -1,12 +1,32 @@
 const googleSearch = require("./script");
 
-dbMock = ["dog.com", "cheesepuff.com", "disney.com", "dogpictures.com"];
-it("this is a trial test", () => {
+dbMock = [
+  "dog.com",
+  "cheesepuff.com",
+  "disney.com",
+  "dogpictures.com",
+  "dogduties.com",
+  "dogbites.com",
+];
+describe("googleSearch", () => {
+  it("this is a trial test", () => {
     expect("hello").toBe("hello");
-  googleSearch("testtest", dbMock);
-});
-it("this is a test", () => {
-   
-  expect(googleSearch("testtest", dbMock)).toEqual([]);
-  expect(googleSearch("dog", dbMock)).toEqual(["dog.com","dogpictures.com"]);
+    googleSearch("testtest", dbMock);
+  });
+
+  it("is searching google", () => {
+    expect(googleSearch("testtest", dbMock)).toEqual([]);
+    expect(googleSearch("dog", dbMock)).toEqual([
+      "dog.com",
+      "dogpictures.com",
+      "dogduties.com",
+    ]);
+  });
+  it("is working with undefined and null", () => {
+    expect(googleSearch(undefined, dbMock)).toEqual([]);
+    expect(googleSearch(null, dbMock)).toEqual([]);
+  });
+  it("doesnot returns more than 3 matches", () => {
+    expect(googleSearch(".com", dbMock).length).toEqual(3);
+  });
 });
